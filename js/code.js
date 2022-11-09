2c77fae752e3f9dbf508c9531d1a6d2fc17168d5
lthurler <leothurler@gmail.com>
2022-11-09 17:47:44 -0300

    ajustes

--- js/code.js
+++ js/code.js
@@ -1,77 +1,63 @@
 // Seleção de elementos
-
 const menuBtn = document.querySelector("#menu");
 const closeMenuBtn = document.querySelector("#close-menu");
 const menu = document.querySelector("#mobile-navbar");
-const desktopLinks =document.querySelectorAll("#navbar a");
+
+const desktopLinks = document.querySelectorAll("#navbar a");
 const mobileLinks = document.querySelectorAll("#mobile-navbar a");
 const allLinks = [...desktopLinks, ...mobileLinks];
+
 const slides = document.querySelectorAll(".banner");
 const dots = document.querySelectorAll(".dot");
-let slideIndex = 0; 
+let slideIndex = 0;
 
 // Funções
-
 function smoothScroll(e) {
+  e.preventDefault();
 
-    e.preventDefault();
-    const href =this.getAttribute("href");
-    const offsetTop =document.querySelector(href).offsetTop;
-    scroll({
-        top: offsetTop,
-        behavior: "smooth",        
-    })
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
 
-    setTimeout(() => {
+  scroll({
+    top: offsetTop,
+    behavior: "smooth",
+  });
 
-        if(menu.classList.contains("menu-active")) {
-            menu.classList.remove("menu-active");
-        }
-    }, 500);
+  setTimeout(() => {
+    if (menu.classList.contains("menu-active")) {
+      menu.classList.remove("menu-active");
+    }
+  }, 500);
 }
 
 function showSlides() {
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("active");
+    dots[i].classList.remove("active");
+  }
 
-    for(let i = 0; i<slides.length; i++) {
-        slides[i].classList.remove("active");
-        dots[i].classList.remove("active");
-    }
+  slideIndex++;
 
-    slideIndex++
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
 
-    if(slideIndex > slides.length) {
-        slideIndex = 1;
-    }
+  slides[slideIndex - 1].classList.add("active");
+  dots[slideIndex - 1].classList.add("active");
 
-    slides[slideIndex -1 ].classList.add("active");
-    dots[slideIndex -1 ].classList.add("active");
-
-    setTimeout(showSlides, 3000);
+  setTimeout(showSlides, 3000);
 }
 
 // Eventos
-
-[menuBtn, closeMenuBtn].forEach((btn) =>{
-
-    btn.addEventListener("click",(e) =>{
-        menu.classList.toggle("menu-active");
-    });    
+[menuBtn, closeMenuBtn].forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    menu.classList.toggle("menu-active");
+  });
 });
 
-// menuBtn.addEventListener("click", (e)=> {
-//     menu.classList.add("menu-active");    
-// });
-
-// closeMenuBtn.addEventListener("click", (e)=> {
-//     menu.classList.remove("menu-active");    
-// });
-
-allLinks.foreach((link) => {
-
-    link.addEventListener("click", smoothScroll);
-})
+allLinks.forEach((link) => {
+  link.addEventListener("click", smoothScroll);
+});
 
 // Inicialização
-
 showSlides();
-
